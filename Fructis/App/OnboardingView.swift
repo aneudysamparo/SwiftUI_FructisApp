35173520ff2e77a7cdbec463475bc56f344f9c6b
Aneudys Amparo <aneudysamparo@gmail.com>
2021-01-02 19:53:09 -0400

    -- Initial Commit -- Initial assets -- Card Fruit -- Onboarding

---
+++ Fructis/App/OnboardingView.swift
@@ -0,0 +1,35 @@
+//
+//  OnboardingView.swift
+//  Fructis
+//
+//  Created by Aneudys Amparo on 2/1/21.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTIES
+    
+    var fruits: [Fruit] = fruitsData
+    
+    
+    // MARK: - BODY
+    var body: some View {
+        TabView{
+            ForEach(fruits[0...5]) { item in
+                FruitCardView(fruit: item)
+            }
+            
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+        
+    }
+}
+
+// MARK: - PREVIEW
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView(fruits: fruitsData)
+    }
+}
---
+++ Fructis/Model/FruitModel.swift
@@ -0,0 +1,20 @@
+//
+//  FruitModel.swift
+//  Fructis
+//
+//  Created by Aneudys Amparo on 2/1/21.
+//
+
+import SwiftUI
+
+// MARK: - FRUIT DATA MODEL
+
+struct Fruit: Identifiable{
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var gradientColors: [Color]
+    var description: String
+    var nutrition: [String]
+}
