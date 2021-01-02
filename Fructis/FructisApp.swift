cf2db1914a746ccf360246729ecd038d9ab69f98
Aneudys Amparo <aneudysamparo@gmail.com>
2021-01-02 18:31:10 -0400

    Initial Commit

---
+++ Fructis/FructisApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructisApp.swift
+//  Fructis
+//
+//  Created by Aneudys Amparo on 2/1/21.
+//
+
+import SwiftUI
+
+@main
+struct FructisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
